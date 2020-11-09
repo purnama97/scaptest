f0fd7eb6b27ca1d0739eda882ee9a573eb5049bf
purnama97 <ipur.purnama351@gmail.com>
2020-11-09 14:40:08 +0700

    Membuat UI dan Perbaikan

--- src/components/GridMovie/GridMovie.jsx
+++ src/components/GridMovie/GridMovie.jsx
@@ -18,7 +18,6 @@ const GridMovie = () => {
         height: '390',
         width: '640',
         playerVars: {
-          // https://developers.google.com/youtube/player_parameters
           autoplay: 0,
         },
     };
